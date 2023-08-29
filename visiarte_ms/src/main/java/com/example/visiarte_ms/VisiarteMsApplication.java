14f76c7763d7e90275913b1d9332efcbbb6725e3
kae lucas <kaeteodoro@gmail.com>
2023-08-29 10:45:35 -0300

    first commit

---
+++ visiarte_ms/src/main/java/com/example/visiarte_ms/VisiarteMsApplication.java
@@ -0,0 +1,13 @@
+package com.example.visiarte_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisiarteMsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VisiarteMsApplication.class, args);
+    }
+
+}
